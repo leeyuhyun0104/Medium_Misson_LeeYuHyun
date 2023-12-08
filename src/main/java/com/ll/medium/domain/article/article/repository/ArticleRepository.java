78eceff5e6f9c1668022402743e991f8b3f2b99f
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-12-08 15:15:40 +0900

    데이터 조회, 수정, 삭제 test

--- src/main/java/com/ll/medium/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/medium/domain/article/article/repository/ArticleRepository.java
@@ -3,6 +3,10 @@ package com.ll.medium.domain.article.article.repository;
 import com.ll.medium.domain.article.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Integer> {
+import java.util.List;
 
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
+    Article findBySubject(String subject); // 제목으로 데이터 조회하는 메서드
+    Article findBySubjectAndContent(String subject, String content); // 제목과 내용으로 조회
+    List<Article> findBySubjectLike(String subject); // 제목에 특정 문자열 포함하는 데이터 조회
 }
--- src/test/java/com/ll/medium/domain/article/article/controller/ArticleControllerTest.java
+++ src/test/java/com/ll/medium/domain/article/article/controller/ArticleControllerTest.java
@@ -2,30 +2,56 @@ package com.ll.medium.domain.article.article.controller;
 
 import com.ll.medium.domain.article.article.entity.Article;
 import com.ll.medium.domain.article.article.repository.ArticleRepository;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class ArticleControllerTest {
     @Autowired
     private ArticleRepository articleRepository;
 
+    @DisplayName("데이터 조회")
     @Test
     void testJpa() {
-        Article a1 = new Article();
-        a1.setSubject("sbb가 무엇인가요?");
-        a1.setContent("sbb에 대해서 알고 싶습니다.");
-        a1.setCreateDate(LocalDateTime.now());
-        this.articleRepository.save(a1);  // 첫번째 질문 저장
+        Article a = this.articleRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1, a.getId());
+        }
 
-        Article a2 = new Article();
-        a2.setSubject("스프링부트 모델 질문입니다.");
-        a2.setContent("id는 자동으로 생성되나요?");
-        a2.setCreateDate(LocalDateTime.now());
-        this.articleRepository.save(a2);  // 두번째 질문 저장
+    @DisplayName("제목에 특정 문자열 포함하는 데이터 조회")
+    @Test
+    void testJpa2() {
+        List<Article> qList = this.articleRepository.findBySubjectLike("sbb%");
+        Article a = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", a.getSubject());
+    }
+
+    @DisplayName("데이터 수정")
+    @Test
+    void testJpa3() {
+        Optional<Article> oa = this.articleRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Article a = oa.get();
+        a.setSubject("수정된 제목");
+        this.articleRepository.save(a);
+    }
+
+    @DisplayName("데이터 삭제")
+    @Test
+    void testJpa4(){
+        assertEquals(2, this.articleRepository.count()); // 삭제 전 데이터가 2개인지 테스트
+        Optional<Article> oa = this.articleRepository.findById(1);
+
+        assertTrue(oa.isPresent());
+        Article a = oa.get();
+        this.articleRepository.delete(a);
+        assertEquals(1, this.articleRepository.count()); // 삭제 후 1개인지 테스트
     }
 }
+
