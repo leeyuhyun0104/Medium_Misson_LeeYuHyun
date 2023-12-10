1c75280096a7a53c3761ad942fb8d3dfd2184c44
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-12-10 21:25:32 +0900

    ArticleService 생성

--- src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/ll/medium/domain/article/article/controller/ArticleController.java
@@ -1,22 +1,22 @@
 package com.ll.medium.domain.article.article.controller;
 
 import com.ll.medium.domain.article.article.entity.Article;
-import com.ll.medium.domain.article.article.repository.ArticleRepository;
+import com.ll.medium.domain.article.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class ArticleController {
-    private final ArticleRepository articleRepository;
+    private final ArticleService articleService;
 
     @GetMapping("/article/list")
     public String list(Model model) {
-        List<Article> articleList = this.articleRepository.findAll(); // findAll 메서드 사용해서 articleList 생성
+        List<Article> articleList = this.articleService.getList(); // getList 메서드 사용해서 articleList 조회
         model.addAttribute("articleList", articleList); // Model 객체에 "articleList"라는 이름으로 값을 저장
         return "domain/article/article/article_list";
     }
---
+++ src/main/java/com/ll/medium/domain/article/article/service/ArticleService.java
@@ -0,0 +1,19 @@
+package com.ll.medium.domain.article.article.service;
+
+import com.ll.medium.domain.article.article.entity.Article;
+import com.ll.medium.domain.article.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public List<Article> getList() {
+        return this.articleRepository.findAll(); // findAll 메서드로 articleList 생성
+    }
+}
+
