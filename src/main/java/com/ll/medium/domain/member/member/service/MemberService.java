c64048c20db83835e7038d35941df79a2e1b6b3c
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-12-12 21:16:51 +0900

    회원가입

---
+++ src/main/java/com/ll/medium/domain/member/member/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.ll.medium.domain.member.member.service;
+
+import com.ll.medium.domain.member.member.entity.Member;
+import com.ll.medium.domain.member.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member join(String username, String password) {
+        Member member = new Member(username, password);
+        member.setUsername(username);
+
+        this.memberRepository.save(member);
+        return member;
+    }
+}
