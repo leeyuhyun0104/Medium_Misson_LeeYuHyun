24716f2d8542aa6b0d7cdf142c2bf5870d840380
leeyuhyun0104 <dldbgus0104@gmail.com>
2023-12-27 00:11:23 +0900

    댓글 수정

--- src/main/java/com/ll/medium/domain/comment/comment/controller/CommentController.java
+++ src/main/java/com/ll/medium/domain/comment/comment/controller/CommentController.java
@@ -2,12 +2,14 @@ package com.ll.medium.domain.comment.comment.controller;
 
 import com.ll.medium.domain.article.article.entity.Article;
 import com.ll.medium.domain.article.article.service.ArticleService;
+import com.ll.medium.domain.comment.comment.entity.Comment;
 import com.ll.medium.domain.comment.comment.entity.CommentForm;
 import com.ll.medium.domain.comment.comment.service.CommentService;
 import com.ll.medium.domain.member.member.entity.Member;
 import com.ll.medium.domain.member.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -43,5 +46,34 @@ public class CommentController {
         }
         this.commentService.create(article, commentForm.getBody(), member);
         return String.format("redirect:/post/%s", id);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/post/{postId}/comment/{commentId}/modify")
+    public String commentModify(CommentForm commentForm, @PathVariable("postId") Integer postId,
+                                @PathVariable("commentId") Integer commentId, Principal principal) {
+        Comment comment = this.commentService.getComment(commentId);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        commentForm.setBody(comment.getBody());
+        return "domain/comment/comment/comment_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/post/{postId}/comment/{commentId}/modify")
+    public String commentModify(@Valid CommentForm commentForm, BindingResult bindingResult,
+                                @PathVariable("postId") Integer postId,
+                                @PathVariable("commentId") Integer commentId,
+                                Principal principal) {
+        if (bindingResult.hasErrors()) {
+            return "domain/comment/comment/comment_form";
+        }
+        Comment comment = this.commentService.getComment(commentId);
+        if (!comment.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        this.commentService.modify(comment, commentForm.getBody());
+        return String.format("redirect:/post/%s", comment.getArticle().getId());
     }
 }
